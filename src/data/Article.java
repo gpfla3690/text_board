1f0f0a0ce522a16f89ebb57d4cefd1fb29d2dd89
gpfla3690 <gpfla3690@gmail.com>
2022-10-02 16:18:41 +0900

    Article MVC 생성

---
+++ src/data/Article.java
@@ -0,0 +1,69 @@
+package data;
+
+import java.time.LocalDateTime;
+
+public class Article {
+
+    private int id;
+
+    private String title;
+    private String body;
+    private String author;
+
+    private LocalDateTime regDate = LocalDateTime.now();
+    private LocalDateTime updateDate = LocalDateTime.now();
+
+    public Article(String title, String body, String author){
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDateTime getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(LocalDateTime regDate) {
+        this.regDate = regDate;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+}
--- src/infra/Container.java
+++ src/infra/Container.java
@@ -1,10 +1,13 @@
 package infra;
 
+import controller.ArticleController;
 import controller.Controller;
 import controller.MemberController;
 import controller.SystemController;
 import data.Member;
+import repository.ArticleRepository;
 import repository.MemberRepository;
+import service.ArticleService;
 import service.MemberService;
 
 import java.util.Scanner;
@@ -14,23 +17,28 @@ public class Container {
     public static Scanner sc;
     public static Session session;
 
-    // repository
     public static MemberRepository memberRepository;
+    public static ArticleRepository articleRepository;
 
-    // service
     public static MemberService memberService;
+    public static ArticleService articleService;
 
-    // controller
     public static SystemController systemController;
     public static MemberController memberController;
+    public static ArticleController articleController;
 
     static {
         sc = new Scanner(System.in);
         session = new Session();
 
         memberRepository = new MemberRepository();
+        articleRepository = new ArticleRepository();
+
         memberService = new MemberService();
+        articleService = new ArticleService();
+
         memberController = new MemberController();
+        articleController = new ArticleController();
         systemController = new SystemController();
     }
 
